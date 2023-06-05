4885f0e8c91ae520ef725db824c666a8a68c5d84
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-06-05 08:54:37 +0530

    Added restaurant ordering app

--- index.js
+++ index.js
@@ -5,105 +5,107 @@ const paymentForm = document.getElementById("payment-form")
 const nameInput = document.getElementById("name-input")
 const paymentStatusEl = document.getElementById("payment-status-el")
 
-document.addEventListener("click", function(e) {
-    if(e.target.dataset.id) {
-        handleAddBtnClick(e.target.dataset.id)
-    } else if(e.target.dataset.remove) {
-        handleRemoveBtnClick(e.target.dataset.remove)
-    } else if(e.target.id === "order-btn") {
-        handleOrderBtnClick()
-    } else if(e.target.id === "pay-btn") {
-        e.preventDefault()
-        handlePayBtnClick()
-    }
+document.addEventListener("click", function (e) {
+	if (e.target.dataset.id) {
+		handleAddBtnClick(e.target.dataset.id)
+	} else if (e.target.dataset.remove) {
+		handleRemoveBtnClick(e.target.dataset.remove)
+	} else if (e.target.id === "order-btn") {
+		handleOrderBtnClick()
+	} else if (e.target.id === "pay-btn") {
+		e.preventDefault()
+		handlePayBtnClick()
+	}
 })
 
 function handleAddBtnClick(productId) {
-    const targetCartObj = menuArray.filter(function(product) {
-        return product.id === Number(productId)
-    })[0]
-    cartData.push(targetCartObj)
-    renderCart()
+	const targetCartObj = menuArray.filter(function (product) {
+		return product.id === Number(productId)
+	})[0]
+	cartData.push(targetCartObj)
+	renderCart()
 }
 
 function handleRemoveBtnClick(removeId) {
-    const targetCartObjIdx = cartData.findIndex(function(product) {
-        return `remove-${product.id}` === removeId
-    })
-    cartData.splice(targetCartObjIdx, 1)
-    renderCart()
+	const targetCartObjIdx = cartData.findIndex(function (product) {
+		return `remove-${product.id}` === removeId
+	})
+	cartData.splice(targetCartObjIdx, 1)
+	renderCart()
 }
 
 function handleOrderBtnClick() {
-    paymentForm.classList.remove("hidden")
+	paymentForm.classList.remove("hidden")
 }
 
 function handlePayBtnClick() {
-    if(nameInput.value) {
-        paymentForm.classList.add("hidden")
-        cartData = []
-        renderCart()
-        paymentStatusEl.classList.remove("hidden")
-        paymentStatusEl.innerHTML = `
-            <p class="payment-greet">Thanks, Sujeet! Your Order is on it's way!</p>
+	if (nameInput.value) {
+		paymentForm.classList.add("hidden")
+		cartData = []
+		renderCart()
+		paymentStatusEl.classList.remove("hidden")
+		paymentStatusEl.innerHTML = `
+            <p class="payment-greet">Thanks, ${
+							document.querySelector("#name-input").value
+						}! Your Order is on it's way!</p>
         `
-        setTimeout(function() {
-            paymentStatusEl.innerHTML = `
+		setTimeout(function () {
+			paymentStatusEl.innerHTML = `
             <p class="payment-greet">Your order is accepted!</p>
         `
-        }, 3000)
-        setTimeout(function() {
-            paymentStatusEl.classList.add("hidden")
-        }, 6000)
-    }
+		}, 3000)
+		setTimeout(function () {
+			paymentStatusEl.classList.add("hidden")
+		}, 6000)
+	}
 }
 
 // render cart data
 function getCartData() {
-    let cartHtml = ""
-    let totalCartPrice = 0
-    if(cartData.length > 0) {
-        cartData.forEach(function(product) {
-            cartHtml += `
+	let cartHtml = ""
+	let totalCartPrice = 0
+	if (cartData.length > 0) {
+		cartData.forEach(function (product) {
+			cartHtml += `
                 <div class="cart-product">
                     <span>${product.name}</span>
                     <span class="remove-btn" data-remove="remove-${product.id}">remove</span>
                     <span>$${product.price}</span>
                 </div>
             `
-            totalCartPrice += product.price
-        })
-    }
-    document.getElementById("total-price").innerHTML = `$${totalCartPrice}`
-    return cartHtml
+			totalCartPrice += product.price
+		})
+	}
+	document.getElementById("total-price").innerHTML = `$${totalCartPrice}`
+	return cartHtml
 }
 
 function renderCart() {
-    if(cartData.length > 0) {
-        document.getElementById("checkout-cont").classList.remove("hidden")
-    } else {
-        document.getElementById("checkout-cont").classList.add("hidden")
-    }
-    document.getElementById("cart").innerHTML = getCartData()
+	if (cartData.length > 0) {
+		document.getElementById("checkout-cont").classList.remove("hidden")
+	} else {
+		document.getElementById("checkout-cont").classList.add("hidden")
+	}
+	document.getElementById("cart").innerHTML = getCartData()
 }
 renderCart()
 
-// render available product 
+// render available product
 function getProductsHtml() {
-    let productsHtml = ""
-    
-    menuArray.forEach(function(product) {
-        let ingredientsString = ""
-        
-        product.ingredients.forEach(function(ingredient, idx) {
-            if(product.ingredients.length - 1 === idx) {
-                ingredientsString += ingredient
-            } else {
-                ingredientsString += `${ingredient},`
-            }
-        })
-        
-        productsHtml += `
+	let productsHtml = ""
+
+	menuArray.forEach(function (product) {
+		let ingredientsString = ""
+
+		product.ingredients.forEach(function (ingredient, idx) {
+			if (product.ingredients.length - 1 === idx) {
+				ingredientsString += ingredient
+			} else {
+				ingredientsString += `${ingredient},`
+			}
+		})
+
+		productsHtml += `
             <div class="product">
                 <div class="product-detail">
                     <div class="product-item">${product.emoji}</div>
@@ -118,11 +120,11 @@ function getProductsHtml() {
                 </span>
             </div class="product">
         `
-    })
-    return productsHtml
+	})
+	return productsHtml
 }
 
 function render() {
-    document.getElementById("products").innerHTML = getProductsHtml()
+	document.getElementById("products").innerHTML = getProductsHtml()
 }
 render()
